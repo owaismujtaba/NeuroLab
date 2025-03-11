01e8af7138ff47be308560ce79069c5b13a6ad09
Owais <owais.mujtaba123@gmail.com>
2025-03-12 00:14:55 +0100

    'feat'

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 from src.dataset.data_reader import DataReader
 
+import pdb
 
 reader = DataReader(subject_id='11')
-
-feat = reader.extract_freq_band_envelope()
+pdb.set_trace()
+eeg_features = reader.extract_freq_band_envelope()
+audio_features = reader._audio_processor()
--- src/dataset/data_reader.py
+++ src/dataset/data_reader.py
@@ -3,10 +3,11 @@ from pathlib import Path
 import numpy as np
 import scipy.signal
 from scipy import fftpack
-from scipy.signal import  hilbert
+from scipy.signal import  hilbert, decimate
 from mne.filter import filter_data
 
 from src.utils.graphics import styled_print
+from src.utils.mel_filterbank import MelFilterBank
 import config as config
 
 import pdb
@@ -35,10 +36,8 @@ class DataReader:
         self.words = np.load(Path(dir, f'P{self.subject_id}_stimuli.npy'))
         self.channels = np.load(Path(dir, f'P{self.subject_id}_channels.npy'))
         
-   
-
-
     def extract_freq_band_envelope(self):
+        styled_print('', 'Extracting EEG Features', 'green')
         sr = config.EEG_SR
         data = scipy.signal.detrend(self.eeg, axis=0)
 
@@ -48,19 +47,15 @@ class DataReader:
 
         data = np.abs(hilbert3(data))
 
-        # Compute the number of windows
         num_windows = int(np.floor((data.shape[0] - config.WIN_LENGHT * sr) / (config.FRAME_SHIFT * sr))) + 1
         feat = np.zeros((num_windows, data.shape[1]))
 
-        # Extract windowed mean features
         for win in range(num_windows):
             start = int(np.floor(win * config.FRAME_SHIFT * sr))
             stop = int(start + config.WIN_LENGHT * sr)
             feat[win, :] = np.mean(data[start:stop, :], axis=0)
-
-        return feat
-
-
+        self.eeg_freq_band_features = feat
+        
 
     def _electrode_shaft_referencing(self):
         """
@@ -86,7 +81,6 @@ class DataReader:
 
         self.eeg = data_esr
 
-
     def _clean_eeg(self):
         styled_print('', 'Cleaning sEEG', 'green')
         exclude_prefixes = ('+', 'E', 'el')  # Unwanted channel prefixes
@@ -99,5 +93,59 @@ class DataReader:
 
         self.eeg = clean_data.astype(np.float64)
         self.channels = clean_channels
+
+    def _audio_processor(self):
+        self.target_sr = 16000
+        audio = decimate(self.audio,int(config.AUDIO_SR / self.target_sr))
+        self.audio = np.int16(audio/np.max(np.abs(audio)) * 32767)  
+    
+    def extract_mel_spectrograms(self,audio):    
+        num_windows=int(np.floor((audio.shape[0]-config.WIN_LENGHT*self.target_sr)/(config.FRAME_SHIFT*self.target_sr)))
+        win = np.hanning(np.floor(config.WIN_LENGHT*self.target_sr + 1))[:-1]
+        spectrogram = np.zeros((num_windows, int(np.floor(config.WIN_LENGHT*self.target_sr / 2 + 1))),dtype='complex')
+        for w in range(num_windows):
+            start_audio = int(np.floor((w*config.FRAME_SHIFT)*self.target_sr))
+            stop_audio = int(np.floor(start_audio+config.WIN_LENGHT*self.target_sr))
+            a = audio[start_audio:stop_audio]
+            spec = np.fft.rfft(win*a)
+            spectrogram[w,:] = spec
+        mfb = MelFilterBank(spectrogram.shape[1], config.N_FILTERS, self.target_sr)
+        spectrogram = np.abs(spectrogram)
+        spectrogram = (mfb.toLogMels(spectrogram)).astype('float')
+
+        '''
+        if spectrogram.shape[0] != self.eeg_features.shape[0]:
+            t_len = np.min([spectrogram.shape[0], self.eeg_features.shape[0]])
+            spectrogram = spectrogram[:t_len, :]
+            self.eeg_features = self.eeg_features[:t_len, :]
+            target = target[:t_len]
+        '''
+
+        self.audio_spectrograms = spectrogram
+    
+    def stack_features(self, features):
+        feat_stacked = np.zeros((
+            features.shape[0] - (2 * config.MODEL_ORDER * config.STEP_SIZE),
+            (2 * config.MODEL_ORDER + 1) * features.shape[1]
+        ))
+
+        for f_num, i in enumerate(
+            range(config.MODEL_ORDER * config.STEP_SIZE, features.shape[0] - config.MODEL_ORDER * config.STEP_SIZE)
+        ):
+            ef = features[i - config.MODEL_ORDER * config.STEP_SIZE : i + config.MODEL_ORDER * config.STEP_SIZE + 1 : config.STEP_SIZE, :]
+            feat_stacked[f_num, :] = ef.flatten()
+
+        return feat_stacked
+
+    def label_speech(self):
+        spec_avg = np.mean(self.audio_spectrograms, axis=1)
+        threshold = (np.max(spec_avg) + np.min(spec_avg)) * 0.45
+        labels = np.where(spec_avg > threshold, 'Speech', 'Silence')
+        
+        self.speech_labels = labels
+
+    def extract_eeg_and_audio_features(self):
+
+
         
         

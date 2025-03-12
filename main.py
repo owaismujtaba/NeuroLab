4bb59715d3f67eafad496f449885c42384b9c1a6
Owais <owais.mujtaba123@gmail.com>
2025-03-12 10:58:31 +0100

    'data'

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ from src.dataset.data_reader import DataReader
 
 import pdb
 
-reader = DataReader(subject_id='11')
-pdb.set_trace()
-eeg_features = reader.extract_freq_band_envelope()
-audio_features = reader._audio_processor()
+reader = DataReader(subject_id='21')
+eeg_features = reader.extract_eeg_and_audio_features()
--- src/dataset/data_reader.py
+++ src/dataset/data_reader.py
@@ -4,7 +4,9 @@ import numpy as np
 import scipy.signal
 from scipy import fftpack
 from scipy.signal import  hilbert, decimate
+import numpy.matlib as matlib
 from mne.filter import filter_data
+from sklearn.preprocessing import LabelEncoder
 
 from src.utils.graphics import styled_print
 from src.utils.mel_filterbank import MelFilterBank
@@ -25,9 +27,12 @@ class DataReader:
         self.words = None
         self.channels = None
 
+        self.target_sr = 16000
+
         self._read_data()
         self._clean_eeg()
         self._electrode_shaft_referencing()
+        self._audio_processor()
         
     def _read_data(self): 
         dir = config.DATA_DIR
@@ -36,27 +41,6 @@ class DataReader:
         self.words = np.load(Path(dir, f'P{self.subject_id}_stimuli.npy'))
         self.channels = np.load(Path(dir, f'P{self.subject_id}_channels.npy'))
         
-    def extract_freq_band_envelope(self):
-        styled_print('', 'Extracting EEG Features', 'green')
-        sr = config.EEG_SR
-        data = scipy.signal.detrend(self.eeg, axis=0)
-
-        data = filter_data(data.T, sr, 70, 170, method="iir").T
-        data = filter_data(data.T, sr, 102, 98, method="iir").T
-        data = filter_data(data.T, sr, 152, 148, method="iir").T
-
-        data = np.abs(hilbert3(data))
-
-        num_windows = int(np.floor((data.shape[0] - config.WIN_LENGHT * sr) / (config.FRAME_SHIFT * sr))) + 1
-        feat = np.zeros((num_windows, data.shape[1]))
-
-        for win in range(num_windows):
-            start = int(np.floor(win * config.FRAME_SHIFT * sr))
-            stop = int(start + config.WIN_LENGHT * sr)
-            feat[win, :] = np.mean(data[start:stop, :], axis=0)
-        self.eeg_freq_band_features = feat
-        
-
     def _electrode_shaft_referencing(self):
         """
         Perform electrode shaft referencing by computing the mean signal
@@ -83,22 +67,63 @@ class DataReader:
 
     def _clean_eeg(self):
         styled_print('', 'Cleaning sEEG', 'green')
-        exclude_prefixes = ('+', 'E', 'el')  # Unwanted channel prefixes
-        valid_indices = [
-            i for i, ch in enumerate(self.channels[:, 0])
-            if not any(ch.startswith(prefix) for prefix in exclude_prefixes)
-        ]
-        clean_data = self.eeg[:, valid_indices]
-        clean_channels = self.channels[valid_indices]
-
-        self.eeg = clean_data.astype(np.float64)
+        clean_data = []
+        clean_channels = []
+        channels = self.channels
+        for i in range(channels.shape[0]):
+            if '+' in channels[i][0]: #EKG/MRK/etc channels
+                continue
+            elif channels[i][0][0] == 'E': #Empty channels
+                continue
+            elif channels[i][0][:2] == 'el': #Empty channels
+                continue
+            elif channels[i][0][0] in ['F','C','T','O','P']: #Other channels
+                continue        
+            else:
+                clean_channels.append(channels[i])
+                clean_data.append(self.eeg[:,i])
+        
+        self.eeg =  np.transpose(np.array(clean_data,dtype="float64"))
         self.channels = clean_channels
 
     def _audio_processor(self):
-        self.target_sr = 16000
+       
         audio = decimate(self.audio,int(config.AUDIO_SR / self.target_sr))
         self.audio = np.int16(audio/np.max(np.abs(audio)) * 32767)  
     
+    def _name_vector(self, elecs):
+        model_order = config.MODEL_ORDER
+        names = matlib.repmat(
+            elecs.astype(np.dtype(('U', 10))), 1, 2 * model_order + 1
+        ).T
+
+        for i, offset in enumerate(range(-model_order, model_order + 1)):
+            names[i, :] = [e[0] + 'T' + str(offset) for e in elecs]
+
+        return names.flatten()
+
+
+    def extract_freq_band_envelope(self):
+        styled_print('', 'Extracting EEG Features', 'green')
+        sr = config.EEG_SR
+        data = scipy.signal.detrend(self.eeg, axis=0)
+
+        data = filter_data(data.T, sr, 70, 170, method="iir").T
+        data = filter_data(data.T, sr, 102, 98, method="iir").T
+        data = filter_data(data.T, sr, 152, 148, method="iir").T
+
+        data = np.abs(hilbert3(data))
+
+        num_windows = int(np.floor((data.shape[0] - config.WIN_LENGHT * sr) / (config.FRAME_SHIFT * sr))) + 1
+        feat = np.zeros((num_windows, data.shape[1]))
+
+        for win in range(num_windows):
+            start = int(np.floor(win * config.FRAME_SHIFT * sr))
+            stop = int(start + config.WIN_LENGHT * sr)
+            feat[win, :] = np.mean(data[start:stop, :], axis=0)
+        self.eeg_freq_band_features = feat
+        return feat
+        
     def extract_mel_spectrograms(self,audio):    
         num_windows=int(np.floor((audio.shape[0]-config.WIN_LENGHT*self.target_sr)/(config.FRAME_SHIFT*self.target_sr)))
         win = np.hanning(np.floor(config.WIN_LENGHT*self.target_sr + 1))[:-1]
@@ -113,15 +138,8 @@ class DataReader:
         spectrogram = np.abs(spectrogram)
         spectrogram = (mfb.toLogMels(spectrogram)).astype('float')
 
-        '''
-        if spectrogram.shape[0] != self.eeg_features.shape[0]:
-            t_len = np.min([spectrogram.shape[0], self.eeg_features.shape[0]])
-            spectrogram = spectrogram[:t_len, :]
-            self.eeg_features = self.eeg_features[:t_len, :]
-            target = target[:t_len]
-        '''
-
         self.audio_spectrograms = spectrogram
+        return spectrogram
     
     def stack_features(self, features):
         feat_stacked = np.zeros((
@@ -141,11 +159,37 @@ class DataReader:
         spec_avg = np.mean(self.audio_spectrograms, axis=1)
         threshold = (np.max(spec_avg) + np.min(spec_avg)) * 0.45
         labels = np.where(spec_avg > threshold, 'Speech', 'Silence')
-        
-        self.speech_labels = labels
+        le = LabelEncoder()
+        le = le.fit(labels)
+        self.speech_labels = le.transform(labels)
 
     def extract_eeg_and_audio_features(self):
+        
+        eeg_features = self.extract_freq_band_envelope()
+        eeg_stacked_features = self.stack_features(eeg_features)
 
+        audio_features = self.extract_mel_spectrograms(self.audio)
+        self.label_speech()
+        
+        target = self.speech_labels[config.MODEL_ORDER*config.STEP_SIZE: self.speech_labels.shape[0]-config.MODEL_ORDER*config.STEP_SIZE]
+        spectrograms =audio_features[config.MODEL_ORDER*config.STEP_SIZE: audio_features.shape[0]-config.MODEL_ORDER*config.STEP_SIZE]
+       
+        if spectrograms.shape[0]!=eeg_stacked_features.shape[0]:
+            t_len = np.min([spectrograms.shape[0],eeg_stacked_features.shape[0]])
+            spectrograms = spectrograms[:t_len,:]
+            eeg_stacked_features = eeg_stacked_features[:t_len,:]
+            target = target[:t_len]
+
+        self.target = target
+        self.eeg_features = eeg_stacked_features
+        self.feature_names = self._name_vector(np.array(self.channels))
+        self.channels = self.channels.flatten()
+        self.audio_features = spectrograms
+        
+        
+        
+        return self.eeg_features, self.audio_features, self.feature_names, self.target, self.channels
+        
 
         
         
